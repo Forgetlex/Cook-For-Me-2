f24aed0f0c7b26c77085c321a59972330d6eac2f
Jordan <jordan.lam-deschamps.1@etsmtl.net>
2018-07-27 12:46:35 -0400

    Fix du caméra rig via VR IK
    VR IK est utilisé plutôt que Final IK

---
+++ Cook For Me/Assets/CopyPos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyPos : MonoBehaviour {
+
+    public GameObject toCopy;
+    public Vector3 appliedTransform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.position = toCopy.transform.position;
+
+        transform.position += appliedTransform;
+
+	}
+}
---
+++ Cook For Me/Assets/UpdateGrabbable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateGrabbable : MonoBehaviour {
+
+    OVRGrabbable grab;
+
+    // Use this for initialization
+    void Start () {
+        grab = gameObject.AddComponent<OVRGrabbable>();
+        grab.DetectGrabPoints();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
