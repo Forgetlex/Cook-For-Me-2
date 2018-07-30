7b5a730113175f33665ac43c1af082587e8e2f85
Jordan <jordan.lam-deschamps.1@etsmtl.net>
2018-07-30 15:45:42 -0400

    Ajout du MusicManager
    Musique en continue + musique aléatoire!

---
+++ Cook For Me/Assets/MusicManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    //Déclaration des variables
+    public AudioSource Source;
+    public AudioClip[] myMusic = new AudioClip[3];
+
+    void Awake()
+    {
+    }
+
+    void Start()
+    {
+        Source = gameObject.AddComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Source.isPlaying)
+            playRandomMusic();
+    }
+
+    void playRandomMusic()
+    {
+        Source.clip = myMusic[Random.Range(0, myMusic.Length)];
+        Source.Play();
+    }
+}
